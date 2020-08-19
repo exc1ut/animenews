11fafacebf03747c2947ab0297e75a8699c950dc
exc1ut <umid19999@gmail.com>
2020-08-19 05:12:28 +0500

    add build

---
+++ build/precache-manifest.a524cf3ac08bf86dcdd558beb922468c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90bba9929b007a6fbb7296c18be6d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b445fef6e866411c4fa",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "3b445fef6e866411c4fa",
+    "url": "/static/js/2.cbd3ec6d.chunk.js"
+  },
+  {
+    "revision": "3162444221693cf9f7d49b57ba2cb084",
+    "url": "/static/js/2.cbd3ec6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e765a1e0cb15db6de57",
+    "url": "/static/js/main.666280e5.chunk.js"
+  },
+  {
+    "revision": "694b958c40a80665bb94",
+    "url": "/static/js/runtime-main.0049c3c2.js"
+  }
+]);
